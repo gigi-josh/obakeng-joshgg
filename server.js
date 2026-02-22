f05f259924eac44f8275917930b6e6f25a6de173
gigi-josh <nehemiahjoshua885@gmail.com>
2026-02-22 20:14:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+
 const server = http.createServer((req, res) => {
- let filePath = req.url === '/' ? 'index.html' : req.url.substring(1);
-const ext = path.extname(filePath);
-const types = {
-  '.html': 'text/html',
-  '.css': 'text/css',
-  '.js': 'text/javascript'
-}
-const file type = types[ext] || 'text/plain';
-fs.readFile(filePath, (err, data) => {
-  if (err) {
-    res.writeHead(500, {'Content-Type': 'text/plain'})
-    res.end("500- Server error");
-    return;
-  } else {
-    res.writeHead(200, {'Content-Type': filePath})
-    res.end(data);
-  }
- })
+  let filePath = req.url === '/' ? 'index.html' : req.url.substring(1);
+  
+  const ext = path.extname(filePath);
+  const types = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript'
+  };
+  
+  const fileType = types[ext] || 'text/plain';
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end("500 - Server error: " + err.message);
+      return;
+    } else {
+      res.writeHead(200, {'Content-Type': fileType});
+      res.end(data);
+    }
+  });
 });
+
 server.listen(process.env.PORT || 3000, '0.0.0.0', () => {
   console.log("Server is currently working!");
   console.log("Stranger website is online");
